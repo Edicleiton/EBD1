015108793b0d22ccf176d961a48846266bbca8c4
Edi <edicleitoncarlos@gmail.com>
2026-04-22 21:25:10 -0300

    Create calcular_media.cpp

---
+++ PROVAS/Recursao/calcular_media.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int calcularMedia(int v[], int tam);
+int soma(int v[], int tam);
+
+int main(){
+
+    int v[] = {5, 10, 15, 20};
+    int tam = sizeof(v)/sizeof(v[0]);
+
+    int resultado = calcularMedia(v, tam);
+
+    cout << resultado << endl;
+
+    return 0;
+}
+int soma(int v[], int tam){
+    int num;
+
+    if(tam > 0){
+        num = (v[tam-1]);
+        return num + soma(v,tam-1);
+    }
+        return 0;
+
+}
+int calcularMedia(int v[], int tam){
+    int r = soma(v, tam) / tam;
+    return r;
+}
